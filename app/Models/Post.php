e51525011876009c605998424f91e89953c5769b
MadJack1906 <cho.suhoond.74@gmail.com>
2024-05-03 13:25:12 +0800

    Add SoftDeletes to Post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,11 +7,12 @@ use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 #[ObservedBy(PostObserver::class)]
 class Post extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'title',
